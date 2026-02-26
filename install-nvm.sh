45074562fba31ce7ce9a1ed75e5a3bd652366115
Grant Van Horn <grant.t.vanhorn@gmail.com>
2026-02-25 20:46:34 -0500

    Update install-nvm.sh with new installation instructions

--- install-nvm.sh
+++ install-nvm.sh
@@ -13,3 +13,11 @@ if ! grep -q 'NVM_DIR' "$HOME/.bashrc" 2>/dev/null; then
     echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> "$HOME/.bashrc"
     echo 'set +h' >> "$HOME/.bashrc"
 fi
+
+# Source nvm for current shell
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Install Node.js via nvm
+nvm install node
+nvm use node
